b2a2006e6bd158e99811c1d61e49e37baece0ef3
John Figueiredo <john@johnfigueiredo.com>
2013-07-01 13:38:28 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeachersPetUpgraded::Application.config.secret_token = 'cd4b9a4545cc567c262e76ec38e4ced315441b72e704d206ed645dfa2a27fe6c39b722ee6e7e44d099e420d1cefb248294a6405a008a3b576a49501af27d3524'
